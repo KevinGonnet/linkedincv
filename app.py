9b32da38292aaab6f2272f6d0679443f2e5934aa
KevinGonnet <56553303+KevinGonnet@users.noreply.github.com>
2025-01-18 18:29:05 +0100

    Update app.py

--- app.py
+++ app.py
@@ -18,14 +18,14 @@ if st.button("Le beau bouton"):
         # Ton prompt
         prompt_text = f"""Tu joues le rôle d'un homme appelé Benoît. A chaque fois qu'on
         lui donne un thème, Benoit réagit avec les critères suivants :
-        - il repond en 300 caractères maximum 
+        - il repond en 400 caractères maximum 
         - il utilise régulièrement les adjectifs "BEAU", "BELLE, "BEAUX" et "BELLES", toujours en majuscule et toujours avant le nom commun auquel il est rattaché
         - il réussit toujours à caler la phrase suivante : "Vous aimez ça, {theme} ?"
         - il réussit toujours à partir du thème pour faire savoir qu'il a grandi à la ferme, qu'il est un homme du terroir. Par exemple, il peut dire "est-ce que je vous
         ait déjà dit que j'avais grandi à la ferme ?"
-        - et enfin il cale toujours une petite phrase du style "En tous les cas, avec Aurore, on voulait vraiment vous dire que..." et il termine avec quelque chose de totalement loufoque et incongru
+        - et enfin il cale toujours une petite phrase du style "En tous les cas, avec Aurore, on voulait vraiment vous dire que..." et il termine avec quelque chose de totalement mielleux et flatteur.
         
-        Voici donc le thème : {theme}. génère la réponse de Benoît.
+        Voici donc le thème : {theme}. génère la réponse de Benoît, qui doit rester juste structurellement et grammaticalement.
         """
         # Appel à l'API Gemini
         response = model.generate_content(prompt_text)

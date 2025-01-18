8ac4e5a903c22966938ddc56fd87d8a976b9367e
KevinGonnet <56553303+KevinGonnet@users.noreply.github.com>
2025-01-18 12:35:06 +0100

    Update app.py

--- app.py
+++ app.py
@@ -11,14 +11,19 @@ model = genai.GenerativeModel("gemini-1.5-flash")
 
 st.title("Présentation LinkedIn")
 
-user_cv = st.text_area("C'est quoi ton métier ?")
+theme = st.text_area("C'est quoi ton métier ?")
 
 if st.button("Générer mon pitch"):
     with st.spinner("Patience, patience..."):
         # Ton prompt
-        prompt_text = f"""Analyse ce métier :
-        {user_cv}
-        Génère un 'À propos' LinkedIn concis, mais extrêmement sarcastique et caustique, en français.
+        prompt_text = f"""Tu joues le rôle d'un homme appelé Benoît. A chaque fois qu'on
+        lui donne un thème, Benoit réagit avec les critères suivants :
+        - il repond en 200 caractères maximum 
+        - il utilise de manière exagérée les adjectifs "BEAU", "BELLE, et leurs dérivés, et toujours en majuscule
+        - il réussit toujours à caler la phrase suivante : "Vous aimez ça, {theme} ?"
+        - il réussit toujours à faire un lien entre le thème et le fait qu'il ait grandi à la ferme, qu'il est un homme du terroir, quitte
+        à ce que ce soit tiré par les cheveux.
+        Voici donc le thème : {theme}. génère la réponse de Benoît.
         """
         # Appel à l'API Gemini
         response = model.generate_content(prompt_text)

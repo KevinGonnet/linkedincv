b6cde82850ab8ced3bbd038cb6b3c2cde65f8e8b
KevinGonnet <56553303+KevinGonnet@users.noreply.github.com>
2025-01-27 06:55:20 +0100

    Update app.py

--- app.py
+++ app.py
@@ -17,8 +17,9 @@ if st.button("Let's go !"):
     with st.spinner("Patience, patience..."):
         # Ton prompt
         prompt_text = f"""Tu t'adresses à un étudiant en bachelor qui a environ 18 ans. Adresse toi à lui/elle comme si tu le connaissais. Tu le tutoie.
-        Ton but c'est de faire le lien entre le thème suivant et ses études, mais c'est toujours tiré par les cheveux et décalé.
-        Voici donc le thème : {theme}. génère la réponse en 300 caractères maximum, qui doit rester juste structurellement et grammaticalement.
+        Ton but c'est de faire le lien tordu entre le thème n°1 suivant et un thème n°2 de ton choix (ses études, la vie d'étudiant, les choses qui concernent 
+        un jeune de 18 ans, la culture rap, le rappeur booba, devenir célèbre). Tu utilise des expressions très street.
+        Voici donc le thème n°1 : {theme}. génère la réponse en 400 caractères maximum, qui doit rester juste structurellement et grammaticalement.
         """
         # Appel à l'API Gemini
         response = model.generate_content(prompt_text)

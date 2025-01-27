3caa4f92e9475653072f6866e8313c70a23f8bd9
KevinGonnet <56553303+KevinGonnet@users.noreply.github.com>
2025-01-27 06:40:54 +0100

    Update app.py

--- app.py
+++ app.py
@@ -9,14 +9,15 @@ genai.configure(api_key=os.environ["GOOGLE_API_KEY"])
 # Choix du modèle
 model = genai.GenerativeModel("gemini-1.5-flash")
 
-st.title("Oh un étudiant de l'IPAC ! On parle de quoi aujourd'hui ?")
+st.title("Oh un étudiant de l'IPAC ! C'est quoi ton thème ?")
 
-if st.button("Let's go !"):
+theme = st.text_area("N'importe quel thème")
+
+if st.button("Le beau bouton"):
     with st.spinner("Patience, patience..."):
         # Ton prompt
-        prompt_text = f"""Tu joues le rôle du footballeur Kylian Mbappé, qui utilise sa manière de parler et ses
-        expressions caractéristiques pour réagir à n'importe quelle thème.        
-        Voici donc le thème : {theme}. génère la réponse de Kylian, qui doit rester juste structurellement et grammaticalement.
+        prompt_text = f"""u joues le rôle du footballeur Kylian Mbappé, qui utilise sa manière de parler et ses expressions caractéristiques pour réagir à n'importe quelle thème.
+        Voici donc le thème : {theme}. génère la réponse de Benoît, qui doit rester juste structurellement et grammaticalement.
         """
         # Appel à l'API Gemini
         response = model.generate_content(prompt_text)

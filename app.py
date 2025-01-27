b088e0d4f480c658b3adc536ceb7e87b9042a584
KevinGonnet <56553303+KevinGonnet@users.noreply.github.com>
2025-01-27 06:35:10 +0100

    Update app.py

--- app.py
+++ app.py
@@ -9,24 +9,14 @@ genai.configure(api_key=os.environ["GOOGLE_API_KEY"])
 # Choix du modèle
 model = genai.GenerativeModel("gemini-1.5-flash")
 
-st.title("Vous aimez ça")
+st.title("Oh un étudiant de l'IPAC ! On parle de quoi aujourd'hui ?")
 
-theme = st.text_area("N'importe quel thème")
-
-if st.button("Le beau bouton"):
+if st.button("Let's go !"):
     with st.spinner("Patience, patience..."):
         # Ton prompt
-        prompt_text = f"""Tu joues le rôle d'un homme appelé Benoît. A chaque fois qu'on
-        lui donne un thème, Benoit réagit avec les critères suivants :
-        - il repond en 400 caractères maximum 
-        - il utilise régulièrement les adjectifs "BEAU", "BELLE, "BEAUX" et "BELLES", toujours en majuscule et toujours avant le nom commun auquel il est rattaché
-        - il réussit toujours à caler la phrase suivante : "Vous aimez ça, {theme} ?"
-        - il réussit toujours à partir du thème pour faire savoir qu'il a grandi à la ferme, qu'il est un homme du terroir. Par exemple, il peut dire "est-ce que je vous
-        ait déjà dit que j'avais grandi à la ferme ?"
-        - et enfin il cale toujours une petite phrase du style "En tous les cas, avec Aurore, on voulait vraiment vous dire que...",
-        et il termine avec quelque chose de totalement mielleux et flatteur mais qui a un rapport avec le thème.
-        
-        Voici donc le thème : {theme}. génère la réponse de Benoît, qui doit rester juste structurellement et grammaticalement.
+        prompt_text = f"""Tu joues le rôle du footballeur Kylian Mbappé, qui utilise sa manière de parler et ses
+        expressions caractéristiques pour réagir à n'importe quelle thème.        
+        Voici donc le thème : {theme}. génère la réponse de Kylian, qui doit rester juste structurellement et grammaticalement.
         """
         # Appel à l'API Gemini
         response = model.generate_content(prompt_text)

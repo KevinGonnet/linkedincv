f60df582d2ec89074617e38e8f2478bb39c6c04e
KevinGonnet <56553303+KevinGonnet@users.noreply.github.com>
2025-01-27 07:05:47 +0100

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ theme = st.text_area("Écris ci-dessous")
 if st.button("Let's go !"):
     with st.spinner("Patience, patience..."):
         # Ton prompt
-        prompt_text = f"""Tu t'adresses à un étudiant en bachelor qui a environ 18 ans. Adresse toi à lui/elle comme si tu le connaissais. Tu le tutoie.
+        prompt_text = f"""Tu t'adresses à un étudiant en bachelor de management du sport qui a environ 18 ans. Adresse toi à lui/elle comme si tu le connaissais. Tu le tutoie.
         Ton but c'est de faire le lien tordu entre le thème suivants et ses études.
         Dans ta réponse, tu intègres subtilement et aléatoirement 3 expressions issues de la liste suivante, pas plus, ni entre guillemets ni en gras : 
         1. Définition Boomer
